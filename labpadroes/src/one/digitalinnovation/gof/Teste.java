d145eac4867a1ac20fb22f129c496b9568d477f6
EwertonM89 <tonmoreira7@gmail.com>
2023-10-18 22:41:28 -0300

    Exemplo de Padrões de Projeto em JAVA 'puro'

---
+++ labpadroes/src/one/digitalinnovation/gof/Teste.java
@@ -0,0 +1,56 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.facade.Facade;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.Comportamento;
+import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.strategy.Robo;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		
+		// testes relacionados ao Design Pattern Singleton
+		
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        
+        SingletonLazyHolder lazyholder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyholder);
+        lazyholder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyholder);
+        
+     // testes relacionados ao Design Pattern Strategy
+        
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+        
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        
+     // testes relacionados ao Facade
+        
+        Facade facade = new Facade();
+        facade.migrarCliente("Ewerton", "59140000");
+	}
+
+}
